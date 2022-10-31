4e4cb04526638b55c9ba6f5e0d79f0b26c7f20bb
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2022-10-31 20:51:49 +0300

    fix: remove unused import

--- android/src/main/java/com/reactnativesystemnavigationbar/SystemNavigationBarModule.java
+++ android/src/main/java/com/reactnativesystemnavigationbar/SystemNavigationBarModule.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
